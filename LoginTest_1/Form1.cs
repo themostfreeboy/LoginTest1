e3091204f2db7cf4bbb320a1a6f00379be59a02e
themostfreeboy <361425474@qq.com>
2018-04-01 13:20:30 +0800

    添加代码

---
+++ LoginTest_1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LoginTest_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string username=this.textBoxUsername.Text.ToString();
+            string userpassword=this.textBoxUserpassword.Text.ToString();
+            string str = "null";
+            if (String.Compare(username, "jxl") == 0 && String.Compare(userpassword, "jxl") == 0)
+            {
+                str = "succeed";
+            }
+            else
+            {
+                str = "fail";
+            }
+            MessageBox.Show(str);
+        }
+    }
+}
